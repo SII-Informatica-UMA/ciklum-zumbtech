ae78b243983cb0f7a6d42a7a36f3715882048a76
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2024-03-29 22:33:41 +0100

    Se pueden introducir los appelidos en el registro + primer intento de conexión al backend

--- ciklum-frontend/src/app/app.component.ts
+++ ciklum-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,36 @@ export class AppComponent {
   togglePanel(): void {
     this.isRightPanelActive = !this.isRightPanelActive;
   }
+
+
+  constructor(private http: HttpClient) { }
+
+  registrarUsuario(nombre: string, email: string, password: string) {
+    const url = 'http://localhost:8080'; // Reemplaza con la URL y el endpoint correctos
+    const body = { 
+      nombre: nombre,
+      apellido1: email,
+      apellido2:email,
+      email:email 
+    }; // Datos del usuario a enviar al backend
+
+    this.http.post(url, body).subscribe(
+      (response) => {
+        console.log('Usuario registrado exitosamente:', response);
+        // Puedes manejar la respuesta del backend aquí, por ejemplo, mostrar un mensaje de éxito al usuario
+      },
+      (error) => {
+        console.error('Error al registrar usuario:', error);
+        // Puedes manejar el error aquí, por ejemplo, mostrar un mensaje de error al usuario
+      }
+    );
+  }
+
+  onClickRegistrar() {
+    const nombre = (document.getElementById('nameR') as HTMLInputElement).value;
+    const email = (document.getElementById('emailR') as HTMLInputElement).value;
+    const password = (document.getElementById('passwordR') as HTMLInputElement).value;
+    this.registrarUsuario(nombre, email, password);
+  }
+
 }
--- ciklum-frontend/src/app/app.module.ts
+++ ciklum-frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
