b4be5913c13c436b577c55f888bde66e3a5e7d55
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2024-04-03 19:06:08 +0200

    Integración a maedias de info/sesión. Hace falta arreglar detalles-sesion.html

--- ciklum-frontend/src/app/app.routes.ts
+++ ciklum-frontend/src/app/app.routes.ts
@@ -4,11 +4,36 @@ import { ForgottenPasswordComponent } from './forgotten-password/forgotten-passw
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ListadoUsuarioComponent } from './listado-usuario/listado-usuario.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { Entrenamiento } from './entrenamientos/entrenamiento';
+import { ContactoSesionComponent } from './detalles-sesion/contacto-sesion/contacto-sesion.component';
+import { SesionesUsuarioComponent } from './sesiones-usuario/sesiones-usuario.component';
+import { TablaInfoSesionComponent } from './detalles-sesion/tabla-info-sesion/tabla-info-sesion.component';
+import { DetallesSesionComponent } from './detalles-sesion/detalles-sesion.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'sesiones',
+    component: SesionesUsuarioComponent
+  },
+  {
+    path: 'detalle',
+    component: DetallesSesionComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoSesionComponent
+  },
+  {
+    path: 'tabla',
+    component: TablaInfoSesionComponent
+  },
+  {
+    path: 'entrenamiento',
+    component: Entrenamiento
   },
   {
     path: 'forgotten-password',
