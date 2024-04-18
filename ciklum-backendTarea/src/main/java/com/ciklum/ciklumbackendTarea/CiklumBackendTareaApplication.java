ccf33acdb2c97fbe64ec9468a9742c090b265377
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2024-04-18 18:01:22 +0200

    Arreglado problema con el repositorio de sesion

--- ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplication.java
+++ ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplication.java
@@ -1,5 +1,8 @@
 package com.ciklum.ciklumbackendTarea;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,9 @@ import java.io.File;
 
 
 @SpringBootApplication
-public class CiklumBackendTareaApplication {
+public class CiklumBackendTareaApplication implements CommandLineRunner {
+	private static Logger LOG = LoggerFactory
+			.getLogger(CiklumBackendTareaApplication.class);
 
 	public static void main(String[] args) {
 		String filePath = "C:\\Users\\emili\\OneDrive\\Escritorio\\Universidad\\3 Carrera\\2º Semestre\\Sistemas Información Internet\\Proyecto_Zumbtech\\ciklum-zumbtech\\ciklum-backendTarea\\schema.sql";
@@ -16,4 +21,14 @@ public class CiklumBackendTareaApplication {
 
 		SpringApplication.run(CiklumBackendTareaApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) {
+		LOG.info("EXECUTING : command line runner");
+
+		for (int i = 0; i < args.length; ++i) {
+			LOG.info("args[{}]: {}", i, args[i]);
+		}
+	}
 }
+
--- ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/repositories/SesionRepository.java
+++ ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/repositories/SesionRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SesionRepository extends JpaRepository<Sesion, Integer> {
-    Optional<Sesion> findBy(int id);
+public interface SesionRepository extends JpaRepository<Sesion, Long> {
+    Optional<Sesion> findById(Long id);
 }
