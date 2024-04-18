393e5c9e6d2fdfc9bf48f69f212c9457061d609f
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2024-04-18 22:05:55 +0200

    Añadido @ToString

--- ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/entities/Sesion.java
+++ ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/entities/Sesion.java
@@ -1,10 +1,7 @@
 package com.ciklum.ciklumbackendTarea.entities;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.EqualsAndHashCode;
+import lombok.*;
+
 import java.sql.*;
 import java.util.List;
 
@@ -15,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @EqualsAndHashCode
+@ToString
 public class Sesion {
     @Id
     @GeneratedValue
