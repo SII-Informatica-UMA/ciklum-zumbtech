777704f6ea282c75a1fc74a46c9d6ccf043bebbf
Emilio Rodrigo Carreira Villalta <emiliorodrigo.ecr@gmail.com>
2024-04-18 13:01:49 +0200

    Añadido iniciador de spring para tarea 2

---
+++ ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplication.java
@@ -0,0 +1,13 @@
+package com.ciklum.ciklumbackendTarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiklumBackendTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiklumBackendTareaApplication.class, args);
+	}
+
+}
---
+++ ciklum-backendTarea/src/test/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciklum.ciklumbackendTarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiklumBackendTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
