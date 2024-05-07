a4713f9c07d373cdec666407a2146b9135b91870
RaulFernandez1 <rfescano@uma.es>
2024-05-07 12:20:10 +0200

    Plantilla Tests

--- ciklum-backendTarea/src/test/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplicationTests.java
+++ ciklum-backendTarea/src/test/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplicationTests.java
@@ -1,13 +1,40 @@
 package com.ciklum.ciklumbackendTarea;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("Test para servicios")
 class CiklumBackendTareaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Nested
+	@DisplayName("Cuando la base de datos está vacía")
+	public class BaseDatosVacia {
+
+		@Test // Delete - sesion
+		@DisplayName("Lanza error cuando elimina una sesion concreta")
+		public void errorConSesionConcreta() {
+		}
+
+	}
+
+	@Nested
+	@DisplayName("Cuando la base de datos está llena")
+	public class BaseDatosLlena {
+
+		@BeforeEach
+		public void introduceDatos() {
+
+		}
+
+
 	}
 
 }
