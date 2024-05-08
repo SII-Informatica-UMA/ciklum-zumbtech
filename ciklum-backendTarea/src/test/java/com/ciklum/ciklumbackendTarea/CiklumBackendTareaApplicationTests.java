343e1508ffbd29c6d86ac57b8570a50fe60cced0
RaulFernandez1 <rfescano@uma.es>
2024-05-08 10:39:55 +0200

    Test delete error hecho

--- ciklum-backendTarea/src/test/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplicationTests.java
+++ ciklum-backendTarea/src/test/java/com/ciklum/ciklumbackendTarea/CiklumBackendTareaApplicationTests.java
@@ -1,14 +1,80 @@
 package com.ciklum.ciklumbackendTarea;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ciklum.ciklumbackendTarea.repositories.SesionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 
-@SpringBootTest
+import java.net.URI;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("Test para servicios")
 class CiklumBackendTareaApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value(value="${local.server.port}")
+	private int port;
+
+	@Autowired
+	private SesionRepository sesionRepo;
+
+
+	private URI uri(String scheme, String host, int port, String ...paths) {
+		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+		UriBuilder ub = ubf.builder()
+				.scheme(scheme)
+				.host(host).port(port);
+		for (String path: paths) {
+			ub = ub.path(path);
+		}
+		return ub.build();
+	}
+
+	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.get(uri)
+				.accept(MediaType.APPLICATION_JSON)
+				.build();
+		return peticion;
+	}
+
+	private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.delete(uri)
+				.build();
+		return peticion;
+	}
+
+	private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.post(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(object);
+		return peticion;
+	}
+
+	private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.put(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(object);
+		return peticion;
+	}
+
 
 	@Test
 	void contextLoads() {
@@ -21,6 +87,11 @@ class CiklumBackendTareaApplicationTests {
 		@Test // Delete - sesion
 		@DisplayName("Lanza error cuando elimina una sesion concreta")
 		public void errorConSesionConcreta() {
+			var peticion = delete("http","localhost",port,"/sesion1");
+
+			var respuesta = restTemplate.exchange(peticion, Void.class);
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
 		}
 
 	}
