528bae863130ecf1f97ae4f623148953e0b4de9b
unknown <0610938075@uma.es>
2024-05-14 16:31:35 +0200

    Agrega habilitación de CORS en ControladorSesion para conectar con la API del profe

---
+++ ciklum-backendTarea/src/main/java/com/ciklum/ciklumbackendTarea/configs/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.ciklum.ciklumbackendTarea.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://jfrchicanog.github.io") // Reemplaza esto con el origen de tu API
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Headers permitidos
+    }
+}
+
